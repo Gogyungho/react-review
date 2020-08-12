92f8600da6d48a9a20d118944eecb323c4127674
gogyungho <you@example.com>
2020-08-13 00:09:36 +0900

    배열에서 항목을 추가하는 방법 1. spread 연산자 사용한 뒤, 원하는 값을 넣어준다. 2. concat을 사용한다. push는 사용하면 안된다. 업데이트가 되지 않기 때문이다.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, {useRef} from 'react';
+import React, {useState, useRef} from 'react';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
 
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username:'',
+    email:'',
+  });
+  const {username, email} = inputs;
+  const onChange = e => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs, 
+      [name]:value
+    });
+  };
+
+  const [users, setUsers] = useState ([
     {
         id: 1,
         username: 'goguard',
@@ -19,18 +33,37 @@ function App() {
         username: 'lip',
         email:'lip@example.com'
     }
-];
+]);
 
 const nextId = useRef(4); // useRef는 특정 DOM을 선택하고 싶을때 사용할 수 있지만, 그 대신에 어떠한 변수를 계속 기억하고 싶을떄 '(4)' 도 사용된다. 
 
 const onCreate = () =>{
-  console.log(nextId.current) //4
+  const user = {
+    id : nextId.current,
+    username,
+    email,
+  }
+  // setUsers([...users, user]) 배열에 데이터를 추가하는 첫번쨰 방법  "spread"
+  setUsers(users.concat(user)) // 베열에 데이터를 추가하는 두번째 방법  "concat" (push는 사용하면 안된다. )
+  setInputs({
+    username:'',
+    email:'',
+  });
+  
   nextId.current +=1 ; // 하지만 이 값이 바뀐다고 해서 컴포넌트가 리랜더링 되는것은 아니다. 
 
 }
  
   return (
-   <UserList users={users} />
+    <>
+    <CreateUser 
+      username={username} 
+      email={email} 
+      onChange={onChange} 
+      onCreate={onCreate} 
+     />
+    <UserList users={users} />
+   </>
   );
 }
 
---
+++ src/CreateUser.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function CreateUser ({ username, email, onChange, onCreate }){  //onChange는 input값이 바뀔때 호출하는 함수, onCreate 버튼을 눌렀을때 새로운 항목을 등록해주는 함수 
+    return(
+        <div>
+            <input 
+                name="username" 
+                placeholder ="계정명" 
+                onChange={onChange} 
+                value={username} 
+             />
+            <input 
+                name="email" 
+                placeholder ="이메일" 
+                onChange={onChange} 
+                value={email} 
+             />
+            <button onClick={onCreate}>등록</button>
+        </div>
+    );
+}
+
+export default CreateUser; 
